8b6f8fac92ca5571661d58e46b184c620b00047a
amadiprecious990-boop <amadiprecious990@gmail.com>
2026-05-22 19:35:40 +0100

    message goes here

--- main.py
+++ main.py
@@ -1,9 +1,9 @@
-name = "precious"
-age = 20
-height = 6.2
-is_onine = True
-#camelCase:  (e.g., firstName, calculateTotalAmount).
-#PascalCase: (e.g., FirstName, CustomerRecord).
-#snake_case:  (e.g., first_name, user_account_id).
-#kebab-case:  (e.g., first-name, button-style).
-#(MACRO_CASE):  (e.g., MAX_RETRIES, API_KEY). Used for global
+name = "precious" #string variable
+age = 20 # integer
+height = 6.2 # float
+is_onine = True #boolean
+# camelCase:  (e.g., firstName, calculateTotalAmount).
+# PascalCase: (e.g., FirstName, CustomerRecord).
+# snake_case:  (e.g., first_name, user_account_id).
+# kebab-case:  (e.g., first-name, button-style).
+# (MACRO_CASE):  (e.g., MAX_RETRIES, API_KEY). Used for global
